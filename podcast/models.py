a828ff05b1d8c919c8fdf8b19f5b42548e99f7e1
Aayush Gauba <gauba.aayush@gmail.com>
2022-09-06 19:03:18 -0500

    Podcast Page and model

--- podcast/models.py
+++ podcast/models.py
@@ -1,3 +1,11 @@
+from typing_extensions import Required
 from django.db import models
 
 # Create your models here.
+class Podcast(models.Model):
+    Date = models.DateField()
+    Audio= models.FileField(upload_to='../podcasts')
+    Summary = models.TextField()
+    Description = models.TextField()
+    Tag = models.CharField(max_length=30)
+

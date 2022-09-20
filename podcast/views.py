839af13214fb5da628242557c7bbdd29d6b5a7de
Aayush Gauba <gauba.aayush@gmail.com>
2022-09-19 22:01:46 -0500

    Linked podcast page to sql database

--- podcast/views.py
+++ podcast/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Podcast
 
 def podcastList(request):
-    return render(request,'podcast/podcasts.html')
-    
+    podcasts =  Podcast.objects.all()
+    return render(request,'podcast/podcasts.html',context={'podcasts':podcasts})
+
+def podcastView(request, podcast_id):
+    podcast =  Podcast.objects.get(id = podcast_id)
+    return render(request,'podcast/podcast.html',context={'podcasts':podcast})
